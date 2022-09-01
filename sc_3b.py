2827083af7c8af2106014dee8663a4bda6c2d5a8
sarkarsoumik <112656330+sarkarsoumik@users.noreply.github.com>
2022-09-02 00:12:42 +0530

    Create sc_3b.py

---
+++ sc_3b.py
@@ -0,0 +1,28 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+H = 70 / 3.086e19
+a0 = 0.0
+a1 = 1.0
+n = 1000
+
+def f(a, t):
+    return a**0.5 / H
+
+def g(a):
+    return 2 * a**1.5 / (3 * H)
+
+h = (a1 - a0) / (n - 1)
+a = np.linspace(a0, a1, n)
+t = np.zeros(n)
+t[0] = 0
+for i in range(n - 1):
+    t[i + 1] = t[i] + h * f(a[i] + 0.5 * h, t[i] + 0.5 * h * f(a[i], t[i]))
+
+plt.title("3.b (CRN-34)")
+plt.plot(t,a,'r-',label='Numerical')
+plt.plot(g(a),a,'b-',label='Analytical')
+plt.xlabel('t (sec.)')
+plt.ylabel('a(t)')
+plt.legend()
+plt.show()
